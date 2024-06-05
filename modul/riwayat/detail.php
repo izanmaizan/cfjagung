856f8470449ec9bde31d6e72a4e0e6acad179341
izanmaizan <izanmaizan@gmail.com>
2024-06-06 06:38:32 +0700

    update riwayat.php

--- modul/riwayat/detail.php
+++ modul/riwayat/detail.php
@@ -1,150 +1,262 @@
-<title>Detail Riwayat - T-corn</title>
+<!DOCTYPE html>
+<html lang="en">
 
-<?php
-if ($_GET['id']) {
-  $arcolor = array(
-    '#FFFFFF',
-    '#CC66FF',
-    '#019AFF',
-    '#00CBFD',
-    '#00FEFE',
-    '#A4F804',
-    '#FFFC00',
-    '#FDCD01',
-    '#FD9A01',
-    '#FB6700'
-  );
-  date_default_timezone_set('Asia/Jakarta');
-  $inptanggal = date('Y-m-d H:i:s');
+<head>
+  <meta charset="UTF-8">
+  <title>Detail Riwayat - T-corn</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f4f4f9;
+      margin: 0;
+      padding: 20px;
+    }
 
-  $arbobot = array('0', '1', '0.8', '0.6', '0.4', '-0.2', '-0.4', '-0.6', '-0.8', '-1');
-  $argejala = array('');
+    .content {
+      background-color: #fff;
+      padding: 20px;
+      border-radius: 10px;
+      box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    }
 
-  // for (
-  //   $i = 0;
-  //   $i < count($_POST['kondisi']);
-  //   $i++
-  // ) {
-  //   $arkondisi = explode("_", $_POST['kondisi'][$i]);
-  //   if (
-  //     strlen($_POST['kondisi'][$i]) > 1
-  //   ) {
-  //     $argejala += array($arkondisi[0] => $arkondisi[1]);
-  //   }
-  // }
+    h2 {
+      color: #333;
+    }
 
+    table {
+      width: 100%;
+      border-collapse: collapse;
+      margin: 20px 0;
+    }
 
-  $sqlkondisi = mysqli_query($conn, "SELECT * FROM kondisi order by id+0");
-  while ($rkondisi = mysqli_fetch_array($sqlkondisi)) {
-    $arkondisitext[$rkondisi['id']] = $rkondisi['kondisi'];
-  }
+    table th,
+    table td {
+      padding: 10px;
+      text-align: left;
+      border: 1px solid #ddd;
+    }
 
-  $sqlpkt = mysqli_query($conn, "SELECT * FROM penyakit order by kode_penyakit+0");
-  while ($rpkt = mysqli_fetch_array($sqlpkt)) {
-    $arpkt[$rpkt['kode_penyakit']] = $rpkt['nama_penyakit'];
-    $ardpkt[$rpkt['kode_penyakit']] = $rpkt['det_penyakit'];
-    $arspkt[$rpkt['kode_penyakit']] = $rpkt['srn_penyakit'];
-    $argpkt[$rpkt['kode_penyakit']] = $rpkt['gambar'];
-  }
+    table th {
+      background-color: #00A65A;
+      color: #fff;
+    }
 
-  $sqlhasil = mysqli_query($conn, "SELECT * FROM hasil where id_hasil=" . $_GET['id']);
-  while ($rhasil = mysqli_fetch_array($sqlhasil)) {
-    $arpenyakit = unserialize($rhasil['penyakit']);
-    $argejala = unserialize($rhasil['gejala']);
-  }
+    table tr:nth-child(even) {
+      background-color: #f2f2f2;
+    }
 
-  $np1 = 0;
-  foreach ($arpenyakit as $key1 => $value1) {
-    $np1++;
-    $idpkt1[$np1] = $key1;
-    $vlpkt1[$np1] = $value1;
-  }
+    table tr:hover {
+      background-color: #ddd;
+    }
 
-  // --------------------- END -------------------------
+    .kondisipilih {
+      font-weight: bold;
+    }
 
-  echo "<div class='content'>
+    .well {
+      background-color: #f5f5f5;
+      border-left: 5px solid #00A65A;
+      padding: 10px;
+      border-radius: 5px;
+      margin-top: 20px;
+    }
+
+    .card-img-top {
+      border-radius: 10px;
+    }
+
+    .box {
+      margin-top: 20px;
+      border-radius: 10px;
+      border: 1px solid #ddd;
+    }
+
+    .box-header {
+      padding: 10px;
+      border-bottom: 1px solid #ddd;
+      background-color: #f8f8f8;
+    }
+
+    .box-body {
+      padding: 10px;
+    }
+
+    .box-info {
+      border-left: 5px solid #00A65A;
+    }
+
+    .box-warning {
+      border-left: 5px solid #f39c12;
+    }
+
+    .box-danger {
+      border-left: 5px solid #dd4b39;
+    }
+
+    .callout {
+      padding: 10px;
+      margin: 20px 0;
+      border-radius: 5px;
+      background-color: #f4f4f4;
+      border-left: 5px solid #ddd;
+    }
+
+    .callout-success {
+      background-color: #dff0d8;
+      border-left: 5px solid #00A65A;
+    }
+
+    button {
+      padding: 10px 20px;
+      background-color: #00A65A;
+      color: #fff;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+      transition: background-color 0.3s ease;
+    }
+
+    button:hover {
+      background-color: #007F45;
+    }
+
+    button:focus {
+      outline: none;
+    }
+  </style>
+</head>
+
+<body>
+
+  <?php
+  if ($_GET['id']) {
+    $arcolor = array(
+      '#FFFFFF',
+      '#CC66FF',
+      '#019AFF',
+      '#00CBFD',
+      '#00FEFE',
+      '#A4F804',
+      '#FFFC00',
+      '#FDCD01',
+      '#FD9A01',
+      '#FB6700'
+    );
+    date_default_timezone_set('Asia/Jakarta');
+    $inptanggal = date('Y-m-d H:i:s');
+
+    $arbobot = array('0', '1', '0.8', '0.6', '0.4', '-0.2', '-0.4', '-0.6', '-0.8', '-1');
+    $argejala = array('');
+
+    $sqlkondisi = mysqli_query($conn, "SELECT * FROM kondisi order by id+0");
+    while ($rkondisi = mysqli_fetch_array($sqlkondisi)) {
+      $arkondisitext[$rkondisi['id']] = $rkondisi['kondisi'];
+    }
+
+    $sqlpkt = mysqli_query($conn, "SELECT * FROM penyakit order by kode_penyakit+0");
+    while ($rpkt = mysqli_fetch_array($sqlpkt)) {
+      $arpkt[$rpkt['kode_penyakit']] = $rpkt['nama_penyakit'];
+      $ardpkt[$rpkt['kode_penyakit']] = $rpkt['det_penyakit'];
+      $arspkt[$rpkt['kode_penyakit']] = $rpkt['srn_penyakit'];
+      $argpkt[$rpkt['kode_penyakit']] = $rpkt['gambar'];
+    }
+
+    $sqlhasil = mysqli_query($conn, "SELECT * FROM hasil where id_hasil=" . $_GET['id']);
+    while ($rhasil = mysqli_fetch_array($sqlhasil)) {
+      $arpenyakit = unserialize($rhasil['penyakit']);
+      $argejala = unserialize($rhasil['gejala']);
+    }
+
+    $np1 = 0;
+    foreach ($arpenyakit as $key1 => $value1) {
+      $np1++;
+      $idpkt1[$np1] = $key1;
+      $vlpkt1[$np1] = $value1;
+    }
+
+    echo "<div class='content'>
     <h2 class='text text-bold'>Hasil Identifikasi📖 &nbsp;&nbsp;<button id='print' onClick='window.print();'
-        data-toggle='tooltip' data-placement='right' title='Klik tombol ini untuk mencetak hasil identifikasi'><i
-          class='fa fa-print'></i> Cetak</button> </h2>
+        data-toggle='tooltip' data-placement='right' title='Klik tombol ini untuk mencetak hasil identifikasi'>Cetak</button> </h2>
     <hr>
-    <table class='table table-bordered table-striped diagnosa'>
-      <th width=8%>No</th>
-      <th width=10%>Kode</th>
-      <th>Gejala yang terlihat pada jagung</th>
-      <th width=20%>Kondisi</th>
+    <table>
+      <tr>
+        <th width='8%'>No</th>
+        <th width='10%'>Kode</th>
+        <th>Gejala yang terlihat pada jagung</th>
+        <th width='20%'>Kondisi</th>
       </tr>";
-  $ig = 0;
-  foreach ($argejala as $key => $value) {
-    $kondisi = $value;
-    $ig++;
-    $gejala = $key;
-    $sql4 = mysqli_query($conn, "SELECT * FROM gejala where kode_gejala = '$key'");
-    $r4 = mysqli_fetch_array($sql4);
-    echo '<tr>
+    $ig = 0;
+    foreach ($argejala as $key => $value) {
+      $kondisi = $value;
+      $ig++;
+      $gejala = $key;
+      $sql4 = mysqli_query($conn, "SELECT * FROM gejala where kode_gejala = '$key'");
+      $r4 = mysqli_fetch_array($sql4);
+      echo '<tr>
         <td>' . $ig . '</td>';
-    echo '<td>G' . str_pad($r4['kode_gejala'], 3, '0', STR_PAD_LEFT) . '</td>';
-    echo '<td><span class="hasil text text-primary">' . $r4['nama_gejala'] . "</span></td>";
-    echo '<td><span class="kondisipilih" style="color:' . $arcolor[$kondisi] . '">' . $arkondisitext[$kondisi] .
-      "</span></td>
+      echo '<td>G' . str_pad($r4['kode_gejala'], 3, '0', STR_PAD_LEFT) . '</td>';
+      echo '<td><span class="hasil text text-primary">' . $r4['nama_gejala'] . "</span></td>";
+      echo '<td><span class="kondisipilih" style="color:' . $arcolor[$kondisi] . '">' . $arkondisitext[$kondisi] .
+        "</span></td>
       </tr>";
-  }
-  $np = 0;
-  foreach ($arpenyakit as $key => $value) {
-    $np++;
-    $idpkt[$np] = $key;
-    $nmpkt[$np] = $arpkt[$key];
-    $vlpkt[$np] = $value;
-  }
-  if ($argpkt[$idpkt[1]]) {
-    $gambar = 'gambar/penyakit/' . $argpkt[$idpkt[1]];
-  } else {
-    $gambar = 'gambar/noimage.png';
-  }
-  echo "
+    }
+    $np = 0;
+    foreach ($arpenyakit as $key => $value) {
+      $np++;
+      $idpkt[$np] = $key;
+      $nmpkt[$np] = $arpkt[$key];
+      $vlpkt[$np] = $value;
+    }
+    if ($argpkt[$idpkt[1]]) {
+      $gambar = 'gambar/penyakit/' . $argpkt[$idpkt[1]];
+    } else {
+      $gambar = 'gambar/noimage.png';
+    }
+    echo "
     </table>
-    <div class='well well-small'><img class='card-img-top img-bordered-sm' style='float:right; margin-left:15px;'
+    <div class='well'><img class='card-img-top' style='float:right; margin-left:15px;'
         src='" . $gambar . "' height=200>
       <h3>Hasil Identifikasi</h3>";
-  echo "<div class='callout callout-default'>Jenis penyakit yang diderita adalah <b>
+    echo "<div class='callout'>Jenis penyakit yang diderita adalah <b>
           <h3 class='text text-success'>" . $nmpkt[1] . "
         </b> / " . round($vlpkt[1] * 100, 2) . " % (" . $vlpkt[1] . ")<br></h3>";
-  echo "</div>
+    echo "</div>
     </div>
-    <div class='box box-info box-solid'>
-      <div class='box-header with-border'>
+    <div class='box box-info'>
+      <div class='box-header'>
         <h3 class='box-title'>Detail</h3>
       </div>
       <div class='box-body'>
         <h4>";
-  echo $ardpkt[$idpkt[1]];
-  echo "</h4>
+    echo $ardpkt[$idpkt[1]];
+    echo "</h4>
       </div>
     </div>
-    <div class='box box-warning box-solid'>
-      <div class='box-header with-border'>
+    <div class='box box-warning'>
+      <div class='box-header'>
         <h3 class='box-title'>Saran</h3>
       </div>
       <div class='box-body'>
         <h4>";
-  echo $arspkt[$idpkt[1]];
-  echo "</h4>
+    echo $arspkt[$idpkt[1]];
+    echo "</h4>
       </div>
     </div>
-    <div class='box box-danger box-solid'>
-      <div class='box-header with-border'>
+    <div class='box box-danger'>
+      <div class='box-header'>
         <h3 class='box-title'>Kemungkinan lain:</h3>
       </div>
       <div class='box-body'>
         <h4>";
-  for (
-    $ipl = 2;
-    $ipl
-    < count($idpkt);
-    $ipl++
-  ) {
-    echo " <h4><i class='fa fa-caret-square-o-right'></i> " . $nmpkt[$ipl] . "</b> / "
-      . round($vlpkt[$ipl], 2) . " % (" . $vlpkt[$ipl] . ")<br></h4>";
-  }
-  echo "</div></div>
+    for ($ipl = 2; $ipl < count($idpkt); $ipl++) {
+      echo " <h4><i class='fa fa-caret-square-o-right'></i> " . $nmpkt[$ipl] . "</b> / "
+        . round($vlpkt[$ipl], 2) . " % (" . $vlpkt[$ipl] . ")<br></h4>";
+    }
+    echo "</div></div>
       </div>";
-} ?>
+  }
+  ?>
+
+</body>
+
+</html>
--- modul/riwayat/riwayat.php
+++ modul/riwayat/riwayat.php
@@ -1,7 +1,95 @@
-<title>Riwayat - T-corn</title>
-<h2 class='text text-bold'>Riwayat Konsultasi⏳</h2>
-<hr>
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Riwayat - T-corn</title>
+    <style>
+    h2 {
+        color: #333;
+        font-weight: bold;
+    }
+
+    .content {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        margin-top: 20px;
+        margin-right: 20px;
+    }
+
+    table th,
+    table td {
+        padding: 10px;
+        text-align: left;
+        border: 1px solid #ddd;
+    }
+
+    table th {
+        background-color: #00A65A;
+        color: #fff;
+    }
+
+    table tr:nth-child(even) {
+        background-color: #f2f2f2;
+    }
+
+    table tr:hover {
+        background-color: #ddd;
+    }
+
+    .box-chart {
+        margin-top: 20px;
+        border-radius: 10px;
+        border: 1px solid #ddd;
+        width: 40%;
+        background-color: #fff;
+    }
+
+    .box-chart-header {
+        padding: 10px;
+        border-bottom: 1px solid #ddd;
+        background-color: #f8f8f8;
+    }
+
+    .box-chart-title {
+        margin: 0;
+        font-weight: bold;
+    }
+
+    .box-chart-body {
+        padding: 10px;
+    }
+
+    .btn {
+        padding: 10px 20px;
+        background-color: #00A65A;
+        color: #fff;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+        text-decoration: none;
+    }
+
+    .btn:hover {
+        background-color: #007F45;
+        color: #fff;
+    }
+
+    .btn:focus {
+        outline: none;
+    }
+    </style>
+</head>
+
+<body>
+
+    <?php
 include "config/fungsi_alert.php";
 $aksi = "modul/riwayat/aksi_hasil.php";
 switch ($_GET['act']) {
@@ -29,168 +117,146 @@ switch ($_GET['act']) {
     $tampil = mysqli_query($conn, "SELECT * FROM hasil ORDER BY id_hasil");
     $baris = mysqli_num_rows($tampil);
     if ($baris > 0) {
-      echo "<div class='row'><div class='col-md-8'><table class='table table-bordered table-striped table-hover riwayat' style='overflow-x=auto' cellpadding='0' cellspacing='0'>
+      echo "<h2>Riwayat Konsultasi⏳</h2><hr>";
+      echo "<div class='content'><table>
           <thead>
             <tr>
               <th>No</th>
               <th>Tanggal</th>
               <th>Penyakit</th>
-              <th nowrap>Nilai CF</th>
-              <th width='21%' class='text-center'>Aksi</th>
+              <th>Nilai CF</th>
+              <th>Aksi</th>
             </tr>
           </thead>
-		  <tbody>
-		  ";
+          <tbody>";
       $hasil = mysqli_query($conn, "SELECT * FROM hasil ORDER BY id_hasil limit $offset,$limit");
       $no = 1;
       $no = 1 + $offset;
       $counter = 1;
       while ($r = mysqli_fetch_array($hasil)) {
         if ($r['hasil_id'] > 0) {
-          if ($counter % 2 == 0)
-            $warna = "dark";
-          else
-            $warna = "light";
+          $warna = ($counter % 2 == 0) ? "dark" : "light";
           echo "<tr class='" . $warna . "'>
-			 <td align=center>$no</td>
-			 <td>$r[tanggal]</td>
-			 <td>" . $arpkt[$r['hasil_id']] . "</td>
-			 <td><span class='label label-default'>" . $r['hasil_nilai'] . "</span></td>
-			 <td align=center>
-			 <a type='button' class='btn btn-default btn-xs' target='_blank' href=riwayat-detail/$r[id_hasil]><i class='fa fa-eye' aria-hidden='true'></i> Detail </a> &nbsp;
-	         </td></tr>";
+             <td align=center>$no</td>
+             <td>$r[tanggal]</td>
+             <td>" . $arpkt[$r['hasil_id']] . "</td>
+             <td>$r[hasil_nilai]</td>
+             <td align=center>
+             <a type='button' class='btn' target='_blank' href=riwayat-detail/$r[id_hasil]>Detail</a>
+             </td></tr>";
           $no++;
           $counter++;
         }
       }
-      echo "</tbody></table></div>";
-      ?>
+      echo "</tbody></table>";
 
-<div class="col-md-4">
-    <div class="box box-success box-solid">
-        <div class="box-header with-border">
+?>
+
+
+    <div class="box-chart">
+        <div class="box-chart-header">
             <i class="fa fa-pie-chart"></i>
-            <h3 class="box-title">Grafik</h3>
-            <div class="box-tools pull-right">
-                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-            </div>
+            <h3 class="box-chart-title">Grafik</h3>
         </div>
-        <div class="box-body">
+        <div class="box-chart-body">
             <div id="donut-chart" class="chart" style="width:100%;height:250px;"></div>
             <hr>
             <div id="legend-container"></div>
         </div>
         <!-- /.box-body-->
     </div>
-</div>
-
-
-<?php
-      echo "</div><div class='col-md-12'><div class='row'><div class=paging>";
-
-      if ($offset != 0) {
-        $prevoffset = $offset - $limit;
-        echo "<span class=prevnext> <a href=index.php?module=riwayat&offset=$prevoffset>Back</a></span>";
-      } else {
-        echo "<span class=disabled>Back</span>"; //cetak halaman tanpa link
-      }
-      //hitung jumlah halaman
-      $halaman = intval($baris / $limit); //Pembulatan
-
-      if ($baris % $limit) {
-        $halaman++;
-      }
-      for ($i = 1; $i <= $halaman; $i++) {
-        $newoffset = $limit * ($i - 1);
-        if ($offset != $newoffset) {
-          echo "<a href=index.php?module=riwayat&offset=$newoffset>$i</a>";
-          //cetak halaman
-        } else {
-          echo "<span class=current>" . $i . "</span>"; //cetak halaman tanpa link
-        }
-      }
-
-      //cek halaman akhir
-      if (!(($offset / $limit) + 1 == $halaman) && $halaman != 1) {
-
-        //jika bukan halaman terakhir maka berikan next
-        $newoffset = $offset + $limit;
-        echo "<span class=prevnext><a href=index.php?module=riwayat&offset=$newoffset>Next</a>";
-      } else {
-        echo "<span class=disabled>Next</span>"; //cetak halaman tanpa link
-      }
-
-      echo "</div></div></div>";
-    } else {
-      echo "<br><b>Data Kosong !</b>";
-    }
-}
-?>
-
-<script>
-$(function() {
+    </div>
 
     <?php
-    //$arr[] = array();
-    
-    $hasilg = mysqli_query($conn, "SELECT hasil_id, count(hasil_id) jlh_id FROM hasil group by hasil_id ORDER BY jlh_id desc");
-    while ($rg = mysqli_fetch_array($hasilg)) {
-      if ($rg['hasil_id'] > 0) {
-        $arr[] = array('label' => '&nbsp;' . $arpkt[$rg['hasil_id']], 'data' => array(array('Penyakit ' . $rg['hasil_id'], $rg['jlh_id'])));
-      }
-    }
-    ?>
-    var donutData = <?php echo json_encode($arr); ?>
-    //      var donutData = [
-    //        {label: 'Series2', data: 30, color: '#3c8dbc'},
-    //        {label: 'Series3', data: 20, color: '#0073b7'},
-    //        {label: 'Series4', data: 50, color: '#00c0ef'}
-    //      ]
+            echo "<div class='col-md-12'><div class='row'><div class=paging>";
 
-    function legendFormatter(label, series) {
-        return '<div class="text text-primary margin4">' + label + ' ' + Math.round(series.percent) + '%';
-    };
-
-    $.plot('#donut-chart', donutData, {
-        series: {
-            pie: {
-                show: true,
-                radius: 1,
-                innerRadius: 0.3,
-                label: {
-                    show: true,
-                    radius: 2 / 3,
-                    formatter: function(label, series) {
-                        return '<div class="badge bg-navy color-pallete">' + Math.round(series
-                            .percent) + '%</div>';
-                    },
-                    threshold: 0.01
-                }
-
+            if ($offset != 0) {
+                $prevoffset = $offset - $limit;
+                echo "<span class=prevnext> <a href=index.php?module=riwayat&offset=$prevoffset>Back</a></span>";
+            } else {
+                echo "<span class=disabled>Back</span>"; //cetak halaman tanpa link
             }
-        },
-        legend: {
-            show: true,
-            container: $("#legend-container"),
-            labelFormatter: legendFormatter,
+//hitung jumlah halaman
+            $halaman = intval($baris / $limit); //Pembulatan
+
+            if ($baris % $limit) {
+                $halaman++;
+            }
+            for ($i = 1; $i <= $halaman; $i++) {
+                $newoffset = $limit * ($i - 1);
+                if ($offset != $newoffset) {
+                    echo "<a href=index.php?module=riwayat&offset=$newoffset>$i</a>";
+//cetak halaman
+                } else {
+                    echo "<span class=current>" . $i . "</span>"; //cetak halaman tanpa link
+                }
+            }
+
+//cek halaman akhir
+            if (!(($offset / $limit) + 1 == $halaman) && $halaman != 1) {
+
+//jika bukan halaman terakhir maka berikan next
+                $newoffset = $offset + $limit;
+                echo "<span class=prevnext><a href=index.php?module=riwayat&offset=$newoffset>Next</a>";
+            } else {
+                echo "<span class=disabled>Next</span>"; //cetak halaman tanpa link
+            }
+
+            echo "</div></div></div>";
+        } else {
+            echo "<br><b>Data Kosong !</b>";
         }
-    })
-    /*
-     * END DONUT CHART
-     */
-
-})
-
-/*
- * Custom Label formatter
- * ----------------------
- */
-function labelFormatter(label, series) {
-    return '<div style="font-size:13px; text-align:center; padding:2px; color: #fff; font-weight: 600;">' +
-        label +
-        '<br>' +
-        Math.round(series.percent) + '%</div>'
 }
-</script>
+?>
+    <script>
+    $(function() {
+
+        <?php
+        $hasilg = mysqli_query($conn, "SELECT hasil_id, count(hasil_id) jlh_id FROM hasil group by hasil_id ORDER BY jlh_id desc");
+        while ($rg = mysqli_fetch_array($hasilg)) {
+            if ($rg['hasil_id'] > 0) {
+                $arr[] = array('label' => '&nbsp;' . $arpkt[$rg['hasil_id']], 'data' => array(array('Penyakit ' . $rg['hasil_id'], $rg['jlh_id'])));
+            }
+        }
+        ?>
+        var donutData = <?php echo json_encode($arr); ?>
+
+        function legendFormatter(label, series) {
+            return '<div class="text text-primary margin4">' + label + ' ' + Math.round(series.percent) + '%';
+        };
+
+        $.plot('#donut-chart', donutData, {
+            series: {
+                pie: {
+                    show: true,
+                    radius: 1,
+                    innerRadius: 0.3,
+                    label: {
+                        show: true,
+                        radius: 2 / 3,
+                        formatter: function(label, series) {
+                            return '<div class="badge bg-navy color-pallete">' + Math.round(series
+                                .percent) + '%</div>';
+                        },
+                        threshold: 0.01
+                    }
+                }
+            },
+            legend: {
+                show: true,
+                container: $("#legend-container"),
+                labelFormatter: legendFormatter,
+            }
+        })
+    })
+
+    function labelFormatter(label, series) {
+        return '<div style="font-size:13px; text-align:center; padding:2px; color: #fff; font-weight: 600;">' +
+            label +
+            '<br>' +
+            Math.round(series.percent) + '%</div>'
+    }
+    </script>
+</body>
+
+</html>
