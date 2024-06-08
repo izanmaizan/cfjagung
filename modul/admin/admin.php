c0839ec680c119ba7f67081b4591da0ab4058861
izanmaizan <izanmaizan@gmail.com>
2024-06-08 14:57:56 +0700

    update

--- modul/admin/admin.php
+++ modul/admin/admin.php
@@ -1,72 +1,71 @@
 <title>Admin - T-corn</title>
 <style>
-table th,
-table td {
-    padding: 10px;
-    text-align: left;
-    border: 1px solid #ddd;
-}
+	table th,
+	table td {
+		padding: 10px;
+		text-align: left;
+		border: 1px solid #ddd;
+	}
 
-table th {
-    background-color: #00A65A !important;
-    color: #fff;
-}
+	table th {
+		background-color: #00A65A !important;
+		color: #fff;
+	}
 
-table tr:nth-child(even) {
-    background-color: #f2f2f2;
-}
+	table tr:nth-child(even) {
+		background-color: #f2f2f2;
+	}
 
-table tr:hover {
-    background-color: #ddd;
-}
+	table tr:hover {
+		background-color: #ddd;
+	}
 </style>
 <?php
 session_start();
 if (!(isset($_SESSION['username']) && isset($_SESSION['password']))) {
-    header('location:index.php');
-    exit();
+	header('location:index.php');
+	exit();
 } else {
-    ?>
-<script Language="JavaScript">
-<!-- 
-function Blank_TextField_Validator() {
-    if (text_form.username.value == "") {
-        alert("Username tidak boleh kosong !");
-        text_form.username.focus();
-        return (false);
-    }
-    if (text_form.password.value == "") {
-        alert("Password tidak boleh kosong !");
-        text_form.password.focus();
-        return (false);
-    }
-    return (true);
-}
-
-function Blank_TextField_Validator_Cari() {
-    if (text_form.keyword.value == "") {
-        alert("Isi dulu keyword pencarian !");
-        text_form.keyword.focus();
-        return (false);
-    }
-    return (true);
-}
--->
-</script>
-<?php
-include "config/fungsi_alert.php";
-$aksi="modul/admin/aksi_admin.php";
-switch($_GET['act']){
-	// Tampil admin
-  default:
-  $offset=$_GET['offset'];
-	//jumlah data yang ditampilkan perpage
-	$limit = 10;
-	if (empty ($offset)) {
-		$offset = 0;
+	?>
+	<script Language="JavaScript">
+	<!-- 
+	function Blank_TextField_Validator() {
+		if (text_form.username.value == "") {
+			alert("Username tidak boleh kosong !");
+			text_form.username.focus();
+			return (false);
+		}
+		if (text_form.password.value == "") {
+			alert("Password tidak boleh kosong !");
+			text_form.password.focus();
+			return (false);
+		}
+		return (true);
 	}
-  $tampil=mysqli_query($conn,"SELECT * FROM admin ORDER BY username");
-	echo "
+
+	function Blank_TextField_Validator_Cari() {
+		if (text_form.keyword.value == "") {
+			alert("Isi dulu keyword pencarian !");
+			text_form.keyword.focus();
+			return (false);
+		}
+		return (true);
+	}
+	</script>
+	<?php
+	include "config/fungsi_alert.php";
+	$aksi = "modul/admin/aksi_admin.php";
+	switch ($_GET['act']) {
+		// Tampil admin
+		default:
+			$offset = $_GET['offset'];
+			//jumlah data yang ditampilkan perpage
+			$limit = 10;
+			if (empty($offset)) {
+				$offset = 0;
+			}
+			$tampil = mysqli_query($conn, "SELECT * FROM admin ORDER BY username");
+			echo "
 	<br>
 	<form method=POST action='?module=admin' name=text_form onsubmit='return Blank_TextField_Validator_Cari()'>
 	<br>
@@ -84,16 +83,16 @@ switch($_GET['act']){
 			</tr>
     	</table>
 	</form>";
-		  	$baris=mysqli_num_rows($tampil);
-	if ($_POST['Go']){
-			$numrows = mysqli_num_rows(mysqli_query($conn,"SELECT * FROM admin where username like '%$_POST[keyword]%'"));
-			if ($numrows > 0){
-				echo "<div class='alert alert-success alert-dismissible'>
+			$baris = mysqli_num_rows($tampil);
+			if ($_POST['Go']) {
+				$numrows = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM admin where username like '%$_POST[keyword]%'"));
+				if ($numrows > 0) {
+					echo "<div class='alert alert-success alert-dismissible'>
                 <h4><i class='icon fa fa-check'></i> Sukses!</h4>
                 Admin yang anda cari di temukan.
               </div>";
-				$i = 1;
-	echo" <table class='table table-bordered' style='overflow-x=auto' cellpadding='0' cellspacing='0'>
+					$i = 1;
+					echo " <table class='table table-bordered' style='overflow-x=auto' cellpadding='0' cellspacing='0'>
           <thead>
             <tr>
               <th>No</th>
@@ -102,35 +101,36 @@ switch($_GET['act']){
               <th width='21%'>Aksi</th>
             </tr>
           </thead>
-		  <tbody>"; 
-	$hasil = mysqli_query($conn,"SELECT * FROM admin where username like '%$_POST[keyword]%'");
-	$no = 1;
-	$counter = 1;
-    while ($r=mysqli_fetch_array($hasil)){
-	if ($counter % 2 == 0) $warna = "light";
-	else $warna = "dark";
-       echo "<tr class='".$warna."'>
+		  <tbody>";
+					$hasil = mysqli_query($conn, "SELECT * FROM admin where username like '%$_POST[keyword]%'");
+					$no = 1;
+					$counter = 1;
+					while ($r = mysqli_fetch_array($hasil)) {
+						if ($counter % 2 == 0)
+							$warna = "light";
+						else
+							$warna = "dark";
+						echo "<tr class='" . $warna . "'>
 			 <td align=center>$no</td>
 	         <td>$r[username]</td>
 	         <td>$r[nama_lengkap]</td>
 			 <td align=center><a type='button' class='btn btn-success margin' href=admin/editadmin/$r[username]><i class='fa fa-pencil-square-o' aria-hidden='true'></i> Ubah </a> &nbsp;
 	          <a type='button' class='btn btn-danger margin' href=\"JavaScript: confirmIt('Anda yakin akan menghapusnya ?','$aksi?module=admin&act=hapus&id=$r[username]','','','','u','n','Self','Self')\" onMouseOver=\"self.status=''; return true\" onMouseOut=\"self.status=''; return true\"><i class='fa fa-trash-o' aria-hidden='true'></i> Hapus</a>
              </td></tr>";
-      $no++;
-	  $counter++;
-    }
-    echo "</tbody></table>";
-			}
-			else{
-				echo "<div class='alert alert-danger alert-dismissible'>
+						$no++;
+						$counter++;
+					}
+					echo "</tbody></table>";
+				} else {
+					echo "<div class='alert alert-danger alert-dismissible'>
                 <h4><i class='icon fa fa-ban'></i> Gagal!</h4>
                 Maaf, Admin yang anda cari tidak ditemukan , silahkan inputkan dengan benar dan cari kembali.
               </div>";
-			}
-		}else{
-	
-	if($baris>0){
-	echo" <table class='table table-bordered' style='overflow-x=auto' cellpadding='0' cellspacing='0'>
+				}
+			} else {
+
+				if ($baris > 0) {
+					echo " <table class='table table-bordered' style='overflow-x=auto' cellpadding='0' cellspacing='0'>
           <thead>
             <tr>
               <th>No</th>
@@ -140,15 +140,17 @@ switch($_GET['act']){
             </tr>
           </thead>
 		  <tbody>
-		  "; 
-	$hasil = mysqli_query($conn,"SELECT * FROM admin ORDER BY username limit $offset,$limit");
-	$no = 1;
-	$no = 1 + $offset;
-	$counter = 1;
-    while ($r=mysqli_fetch_array($hasil)){
-	if ($counter % 2 == 0) $warna = "dark";
-	else $warna = "light";
-       echo "<tr class='".$warna."'>
+		  ";
+					$hasil = mysqli_query($conn, "SELECT * FROM admin ORDER BY username limit $offset,$limit");
+					$no = 1;
+					$no = 1 + $offset;
+					$counter = 1;
+					while ($r = mysqli_fetch_array($hasil)) {
+						if ($counter % 2 == 0)
+							$warna = "dark";
+						else
+							$warna = "light";
+						echo "<tr class='" . $warna . "'>
 			 <td align=center>$no</td>
 	         <td>$r[username]</td>
 	         <td>$r[nama_lengkap]</td>
@@ -156,56 +158,53 @@ switch($_GET['act']){
 			 <a type='button' class='btn btn-success margin' href=admin/editadmin/$r[username]><i class='fa fa-pencil-square-o' aria-hidden='true'></i> Ubah </a> &nbsp;
 	          <a type='button' class='btn btn-danger margin' href=\"JavaScript: confirmIt('Anda yakin akan menghapusnya ?','$aksi?module=admin&act=hapus&id=$r[username]','','','','u','n','Self','Self')\" onMouseOver=\"self.status=''; return true\" onMouseOut=\"self.status=''; return true\"><i class='fa fa-trash-o' aria-hidden='true'></i> Hapus</a>
              </td></tr>";
-      $no++;
-	  $counter++;
-    }
-    echo "</tbody></table>";
-	echo "<div class=paging>";
+						$no++;
+						$counter++;
+					}
+					echo "</tbody></table>";
+					echo "<div class=paging>";
 
-	if ($offset!=0) {
-		$prevoffset = $offset-10;
-		echo "<span class=prevnext> <a href=index.php?module=admin&offset=$prevoffset>Back</a></span>";
-	}
-	else {
-		echo "<span class=disabled>Back</span>";//cetak halaman tanpa link
-	}
-	//hitung jumlah halaman
-	$halaman = intval($baris/$limit);//Pembulatan
+					if ($offset != 0) {
+						$prevoffset = $offset - 10;
+						echo "<span class=prevnext> <a href=index.php?module=admin&offset=$prevoffset>Back</a></span>";
+					} else {
+						echo "<span class=disabled>Back</span>";//cetak halaman tanpa link
+					}
+					//hitung jumlah halaman
+					$halaman = intval($baris / $limit);//Pembulatan
 
-	if ($baris%$limit){
-		$halaman++;
-	}
-	for($i=1;$i<=$halaman;$i++){
-		$newoffset = $limit * ($i-1);
-		if($offset!=$newoffset){
-			echo "<a href=index.php?module=admin&offset=$newoffset>$i</a>";
-			//cetak halaman
-		}
-		else {
-			echo "<span class=current>".$i."</span>";//cetak halaman tanpa link
-		}
-	}
+					if ($baris % $limit) {
+						$halaman++;
+					}
+					for ($i = 1; $i <= $halaman; $i++) {
+						$newoffset = $limit * ($i - 1);
+						if ($offset != $newoffset) {
+							echo "<a href=index.php?module=admin&offset=$newoffset>$i</a>";
+							//cetak halaman
+						} else {
+							echo "<span class=current>" . $i . "</span>";//cetak halaman tanpa link
+						}
+					}
 
-	//cek halaman akhir
-	if(!(($offset/$limit)+1==$halaman) && $halaman !=1){
+					//cek halaman akhir
+					if (!(($offset / $limit) + 1 == $halaman) && $halaman != 1) {
 
-		//jika bukan halaman terakhir maka berikan next
-		$newoffset = $offset + $limit;
-		echo "<span class=prevnext><a href=index.php?module=admin&offset=$newoffset>Next</a>";
-	}
-	else {
-		echo "<span class=disabled>Next</span>";//cetak halaman tanpa link
-	}
-	
-	echo "</div>";
-	}else{
-	echo "<br><b>Data Kosong !</b>";
-	}
-	}
-    break;
-  
-  case "tambahadmin":
-	echo "<form name=text_form method=POST action='$aksi?module=admin&act=input' onsubmit='return Blank_TextField_Validator()'>
+						//jika bukan halaman terakhir maka berikan next
+						$newoffset = $offset + $limit;
+						echo "<span class=prevnext><a href=index.php?module=admin&offset=$newoffset>Next</a>";
+					} else {
+						echo "<span class=disabled>Next</span>";//cetak halaman tanpa link
+					}
+
+					echo "</div>";
+				} else {
+					echo "<br><b>Data Kosong !</b>";
+				}
+			}
+			break;
+
+		case "tambahadmin":
+			echo "<form name=text_form method=POST action='$aksi?module=admin&act=input' onsubmit='return Blank_TextField_Validator()'>
           <br><br><table class='table table-bordered'>
 		  <tr><td>Nama Lengkap</td>   <td>  <input autocomplete='off' placeholder='Masukkan nama lengkap...' type=text class='form-control' name='nama_lengkap' size=30></td></tr>
           <tr><td>Username</td>   <td>  <input autocomplete='off' placeholder='Masukkan username...' type=text class='form-control' name='username' size=30></td></tr>
@@ -215,13 +214,13 @@ switch($_GET['act']){
 		  <input class='btn btn-danger' type=button name=batal value='Batal' onclick=\"window.location.href='?module=admin';\">
 		  </td></tr>
           </table></form>";
-     break;
-    
-  case "editadmin":
-    $edit=mysqli_query($conn,"SELECT * FROM admin WHERE username='$_GET[id]'");
-    $r=mysqli_fetch_array($edit);
-	
-    echo "<form name=text_form method=POST action='$aksi?module=admin&act=update' onsubmit='return Blank_TextField_Validator()'>
+			break;
+
+		case "editadmin":
+			$edit = mysqli_query($conn, "SELECT * FROM admin WHERE username='$_GET[id]'");
+			$r = mysqli_fetch_array($edit);
+
+			echo "<form name=text_form method=POST action='$aksi?module=admin&act=update' onsubmit='return Blank_TextField_Validator()'>
           <input type=hidden name=id value='$r[username]'>
           <br><br><table class='table table-bordered'>
 	      <tr><td>Username</td> <td>  <input autocomplete='off' type=text class='form-control' name='username' value=\"$r[username]\" size=30></td></tr>
@@ -230,8 +229,8 @@ switch($_GET['act']){
 		  <input class='btn btn-success' type=submit name=submit value='Simpan' >
 		  <input class='btn btn-danger' type=button name=batal value='Batal' onclick=\"window.location.href='?module=admin';\"></td></tr>
           </table></form>";
-    break;  
-}
+			break;
+	}
 ?>
-<?php 
+<?php
 } ?>
